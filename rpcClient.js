b675b9a946379b79ce32937e5e00fe962f2a4dd8
Peter Kovacs <kiiwep@gmail.com>
2015-07-31 11:18:00 +0200

    client publishing

--- rpcClient.js
+++ rpcClient.js
@@ -2,9 +2,10 @@
 
 require('harmony-reflect');
 const amqp = require('amqplib');
+const Hashids = require('hashids');
 
 const rpcQueueOptions = {durable: false};
-const callbackQueueOptions = {durable: false};
+const replyToQueueOptions = {durable: false};
 let amqpChannel;
 let localClients = {};
 
@@ -21,33 +22,55 @@ amqp.connect('amqp://localhost')
 class RPCClient {
 
     constructor(serviceName) {
+        let self = this;
         this.serviceName = serviceName;
-        this.queue = `rpc.queue.${serviceName}`;
+        this.rpcQueueName = `rpc.queue.${serviceName}`;
+        this.functionQueue = {};
 
         if (amqpChannel && !this.queuesRegistered) {
             this.registerQueues();
         }
 
         this.proxy = new Proxy({}, {
-            get(targetObj, propKey) {
+            get(targetObj, method) {
                 return function () {
-                    return new Promise(function (resolve) {
-                        amqpChannel.consume(this.queue);
-                        resolve();
+                    return new Promise(function (resolve, reject) {
+                        let uid = self.generateUID();
+                        self.functionQueue[uid] = {
+                            uid,
+                            method,
+                            arguments: Array.prototype.slice.call(arguments),
+                            sent: false,
+                            resolve,
+                            reject
+                        };
+
+                        if (self.queuesRegistered) {
+                            self.processFunctionQueue();
+                        }
                     });
                 };
             }
         });
     }
 
+    generateUID() {
+        if (!this.hasher) {
+            this.hasher = new Hashids(this.rpcQueueName);
+            this.hashCounter = 0;
+        }
+        return this.hasher.encode(this.hashCounter++, Date.now(), Math.round(Math.random() * 10000));
+    }
+
     registerQueues() {
         let self = this;
         amqpChannel
-            .assertQueue(this.queue, rpcQueueOptions)
-            .then(() => amqpChannel.assertQueue(null, callbackQueueOptions))
-            .then(callbackQueueData => {
-                self.callbackQueueName = callbackQueueData.queue;
+            .assertQueue(this.rpcQueueName, rpcQueueOptions)
+            .then(() => amqpChannel.assertQueue(null, replyToQueueOptions))
+            .then(replyToQueueData => {
+                self.replyToQueueName = replyToQueueData.queue;
                 self.queuesRegistered = true;
+                self.processFunctionQueue();
             });
     }
 
@@ -55,9 +78,27 @@ class RPCClient {
         if (this.queuesRegistered) {
             let self = this;
             amqpChannel
-                .deleteQueue(this.callbackQueueName)
+                .deleteQueue(this.replyToQueueName)
                 .then(() => self.queuesRegistered = false);
         }
+    }
+
+    processFunctionQueue() {
+        let self = this;
+        Object.keys(this.functionQueue).forEach(uid => {
+            let def = self.functionQueue[uid];
+            if (!def.sent) {
+                def.sent = new Date();
+                amqpChannel.publish('', self.rpcQueueName, new Buffer(JSON.stringify({
+                    method: def.method,
+                    arguments: def.arguments
+                })), {
+                    correlationId: uid,
+                    replyTo: self.replyToQueueName
+                });
+                console.log('sent > ', uid);
+            }
+        });
     }
 
 }
