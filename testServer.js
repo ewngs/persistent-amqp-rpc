dedad64e1111eaa1b2e354cfdae672b2e4c10a01
Igor Mucsicska <mucsi_96@yahoo.com>
2015-07-31 10:36:02 +0200

    Server alpha version ready

--- testServer.js
+++ testServer.js
@@ -1,18 +1,3 @@
-//
-// const amqp = require('amqplib');
-//
-// const queueName = 'testQueue';
-// const queueOptions = {durable: false};
-//
-// const link = amqp.connect('amqp://localhost');
-//
-// link.then(function (connection) {
-//   return connection.createChannel();
-// }).then(function (channel) {
-//   return channel.assertQueue(queueName, queueOptions).then(function () {
-//     channel.sendToQueue(queueName, new Buffer('this is a message'));
-//   });
-// });
 'use strict';
 
 function someAsyncOveration(param) {
